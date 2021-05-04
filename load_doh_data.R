f35e6daad055fd0146e2848f6390b4b54090768f
Eli Kern <eli.kern@kingcounty.gov>
2021-05-04 14:17:19 -0700

    Create load_doh_data.R

---
+++ load_doh_data.R
@@ -0,0 +1,25 @@
+#### Purpose: Load DOH-assigned data from REDCap
+# Eli Kern, PHSKC (APDE)
+# 2021-05
+
+#### STEP 1: Set tokens and file paths ####
+#Set REDCap API tokens
+redcap_uri <- "https://redcap.iths.org/api/"
+#keyring::key_set("token_doh_cc") #Run once per machine to store API token using keyring package
+
+
+#### STEP 2: Bring in REDCap data from "Care Coordination - COVID19" project ####
+#These are all DOH assigned cases/households
+#Record ID is household phone #
+#Run time: ~1 min
+col_types <- cols(.default = col_character())
+
+system.time(doh_cc_current <- REDCapR::redcap_read(
+  redcap_uri = redcap_uri,
+  token = keyring::key_get("token_doh_cc"), #Retrieve Eli's token for "Care Coordination - COVID19" Project from keyring package
+  batch_size = 5000L,
+  interbatch_delay = 1,
+  col_types = col_types
+)$data)
+
+rm(col_types)
