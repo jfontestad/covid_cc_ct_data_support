42e4531e471783d60bd64c25b2f5c33aab3f89b6
Eli Kern <eli.kern@kingcounty.gov>
2021-05-06 10:32:55 -0700

    Update cc_data_analysis_master_script.R

--- cc_data_analysis_master_script.R
+++ cc_data_analysis_master_script.R
@@ -34,7 +34,7 @@ if(kc_cc_archive_current_coldiff == 0) {
 
 
 #### STEP 2: Load care coordination data for DOH-assigned cases ####
-devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/covid_cc_ct_data_support/main/load_doh_data.R")
+if(qa_1 == TRUE) {devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/covid_cc_ct_data_support/main/load_doh_data.R")}
 
 #QA: Check to make sure DOH data loaded
 if(exists("doh_cc_current")) {
@@ -45,7 +45,7 @@ if(exists("doh_cc_current")) {
 
 
 #### STEP 3: Clean and combine case, contact and household COVID-19-related data for PHSKC-assigned and DOH-assigned households ####
-devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/covid_cc_ct_data_support/main/clean_ct_data.R")
+if(qa_2 == TRUE) {devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/covid_cc_ct_data_support/main/clean_ct_data.R")}
 
 #QA: Check to make sure all data has been processed, and that household data is distinct at household level
 if(exists("complete_cc_household") &
@@ -59,7 +59,7 @@ if(exists("complete_cc_household") &
 
 
 #### STEP 4: Clean and combine referral data for PHSKC-assigned and DOH-assigned households, include demographic data ####
-devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/covid_cc_ct_data_support/main/clean_referral_data.R")
+if(qa_3 == TRUE) {devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/covid_cc_ct_data_support/main/clean_referral_data.R")}
 
 #QA: Check to make sure all referral data has been processed, and that demographic data is distinct at household level
 if(exists("complete_referral_form_with_demo") &
@@ -73,7 +73,7 @@ if(exists("complete_referral_form_with_demo") &
 
 
 #### STEP 5: Combine cleaned case/contact/household and referral data ####
-devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/covid_cc_ct_data_support/main/clean_referral_data.R")
+if(qa_4 == TRUE) {devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/covid_cc_ct_data_support/main/combine_ct_referral_data.R")}
 
 #QA: Check to make sure all data has been processed, and that demographic data is distinct at household level
 if(exists("final_data_by_household") &
@@ -85,7 +85,7 @@ if(exists("final_data_by_household") &
 
 
 #### STEP 6: Create CHW-level datasets for tabulation and analysis ####
-devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/covid_cc_ct_data_support/main/chw_level_data.R")
+if(qa_5 == TRUE) {devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/covid_cc_ct_data_support/main/chw_level_data.R")}
 
 #QA: Check to make sure all data has been processed, and that analytic dataset is distinct at agency, arm, record_id, referral_timedt level 
 if(exists("complete_referrals_by_chw") &
@@ -97,11 +97,11 @@ if(exists("complete_referrals_by_chw") &
 }
 
 #### STEP 7: Export analytic datasets ####
-devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/covid_cc_ct_data_support/main/export_analytic.R")
+if(qa_6 == TRUE) {devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/covid_cc_ct_data_support/main/export_analytic.R")}
 
 
 #### STEP 8: Tabulate and export for Tableau ####
-devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/covid_cc_ct_data_support/main/export_tableau.R")
+if(qa_6 == TRUE) {devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/covid_cc_ct_data_support/main/export_tableau.R")}
 
 #QA: Check to make tabulated data has been processed
 if(exists("complete_referrals_by_ct_date_tableau") &
@@ -110,3 +110,91 @@ if(exists("complete_referrals_by_ct_date_tableau") &
 } else {
   qa_7 <- FALSE
 }
+
+
+#### STEP 9: Prepare final QA message and send email ####
+
+# Set up email credentials (have to run this once to store credentials securely using keyring package, must be rerun each time password is updated)
+# create_smtp_creds_key(id = "outlook", user = "eli.kern@kingcounty.gov", provider = "outlook", overwrite = T)
+
+#Get a nicely formatted date/time string
+date_time <- add_readable_time()
+date <- Sys.Date()
+
+#Check final QA check
+if(qa_7 == TRUE) {
+  qa_final <- "PASS"
+} else {
+  qa_final <- "FAIL"
+}
+
+#Create email body using dynamic code
+if (qa_final == "PASS") {
+  qa_result <- "**PASS**"
+  qa_checkpoint_message <- "7 of 7 checkpoints completed."
+  
+  chw_analytic_file_demo_message <- paste0("CHW referral demographic QA for referrals between **", demo_qa_start, " - ", demo_qa_end, ":**")
+  kc_analytic_file_message_race <- paste0("* PHSKC-assigned CHW referrals missing race/ethnicity: ",
+                                           kc_raceethnicity_missing_count, " referrals (",
+                                           kc_raceethnicity_missing_per, "%)")
+  kc_analytic_file_message_language <- paste0("* PHSKC-assigned CHW referrals missing language: ",
+                                              kc_language_missing_count, " referrals (",
+                                              kc_language_missing_per, "%)")
+  kc_analytic_file_message_dob <- paste0("* PHSKC-assigned CHW referrals missing date of birth: ",
+                                         kc_dob_missing_count, " referrals (",
+                                         kc_dob_missing_per, "%)")
+  doh_analytic_file_message_race <- paste0("* DOH-assigned CHW referrals missing race/ethnicity: ",
+                                          doh_raceethnicity_missing_count, " referrals (",
+                                          doh_raceethnicity_missing_per, "%)")
+  doh_analytic_file_message_language <- paste0("* DOH-assigned CHW referrals missing language: ",
+                                              doh_language_missing_count, " referrals (",
+                                              doh_language_missing_per, "%)")
+  doh_analytic_file_message_dob <- paste0("* DOH-assigned CHW referrals missing date of birth: ",
+                                         doh_dob_missing_count, " referrals (",
+                                         doh_dob_missing_per, "%)")
+} else {
+  qa_result <- "**FAIL**"
+  qa_checkpoint_message <- NULL
+  chw_analytic_file_demo_message <- NULL
+  kc_analytic_file_message_race <- NULL
+  kc_analytic_file_message_language <- NULL
+  kc_analytic_file_message_dob <- NULL
+  doh_analytic_file_message_race <- NULL
+  doh_analytic_file_message_language <- NULL
+  doh_analytic_file_message_dob <- NULL
+}
+
+#Set subject of email
+subject <- paste0("QA result: ", qa_result, ", COVID-19 Care Coordination metrics, ", date)
+
+# Compose email
+email <-
+  compose_email(
+    body = md(
+      c("Automated update to: ",
+        "* COVID-19 Care Coordination metrics datasets", "\n",
+        "* complete_referrals_analytic_dataset.xlsx", "\n",
+        "* complete_referrals_chw_analytic_dataset.xlsx", "\n",
+        "QA result: ", qa_result, "\n",
+        qa_checkpoint_message, "\n",
+        chw_analytic_file_demo_message, "\n",
+        kc_analytic_file_message_race, "\n",
+        kc_analytic_file_message_language, "\n",
+        kc_analytic_file_message_dob, "\n",
+        doh_analytic_file_message_race, "\n",
+        doh_analytic_file_message_language, "\n",
+        doh_analytic_file_message_dob, "\n"
+      )),
+    footer = md(
+      c("Email sent on ", date_time, ".")
+    )
+  )
+
+# Send email
+smtp_send(email = email,
+          to = c("eli.kern@kingcounty.gov"),
+          #to = c("eli.kern@kingcounty.gov", "krijohnson@kingcounty.gov", "SHernandez@kingcounty.gov"),
+          from = creds_key("outlook")$user,
+          subject = subject,
+          credentials = creds_key("outlook")
+)
