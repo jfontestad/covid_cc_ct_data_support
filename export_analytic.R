d35d03cd2546e7df905b53770b2de7822955a171
Eli Kern <eli.kern@kingcounty.gov>
2021-05-08 15:09:53 -0700

    Added HAR dataset to exports

--- export_analytic.R
+++ export_analytic.R
@@ -16,3 +16,9 @@ data_chw_analytic <- list(complete_chw_analytic)
 sheet_chw_analytic <- list("complete_chw_analytic")
 filename_chw_analytic <- file.path(analytic_export, "complete_referrals_chw_analytic_dataset.xlsx")
 system.time(write.xlsx(data_chw_analytic, file = filename_chw_analytic, sheetName = sheet_chw_analytic))
+
+#### STEP 3: Export HAR analytic dataset ####
+data_har_analytic <- list(complete_har_form_with_demo)
+sheet_har_analytic <- list("complete_har_analytic")
+filename_har_analytic <- file.path(analytic_export, "complete_har_analytic_dataset.xlsx")
+system.time(write.xlsx(data_har_analytic, file = filename_har_analytic, sheetName = sheet_har_analytic))
