0c22904986c06b36378bffaad5cdd1704d86456c
Eli Kern <eli.kern@kingcounty.gov>
2021-05-26 15:46:24 -0700

    Adding new columns to archive PHSKC CT data

--- load_kc_data.R
+++ load_kc_data.R
@@ -124,13 +124,9 @@ kc_cc_archive <- kc_cc_archive_raw %>%
   select(-grocery_order_form_timestamp, -redcap_survey_identifier)
   
   #add extra vars that have not yet made it into
-  if("har_incoming_calls_followup" %in% colnames(kc_cc_archive) == FALSE) {
-    kc_cc_archive <- kc_cc_archive %>%
-      mutate(
-        har_incoming_calls_followup = NA_character_
-      )
-  }
-
+  if("har_incoming_calls_followup" %in% colnames(kc_cc_archive) == FALSE) {kc_cc_archive <- kc_cc_archive %>% mutate(har_incoming_calls_followup = NA_character_)}
+  if("call_arm3_endiq" %in% colnames(kc_cc_archive) == FALSE) {kc_cc_archive <- kc_cc_archive %>% mutate(call_arm3_endiq = NA_character_)}
+  if("vx_assist_arm3" %in% colnames(kc_cc_archive) == FALSE) {kc_cc_archive <- kc_cc_archive %>% mutate(vx_assist_arm3 = NA_character_)}
 
 #### STEP 4: Compare columns in current and archived Household Contact Tracing data, and bind
 kc_cc_archive_current_coldiff <- nrow(janitor::compare_df_cols(kc_cc_archive, kc_cc_current, return = "mismatch", bind_method = "rbind"))
